e6f82af6589b1103b3bdc3e27b104b3955b40bd8
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-01-29 17:54:25 -0500

    completed strings lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Bob";
+
+const welcomeMessage = `${currentUser} Welcome to Flatbook, !` ;
+
+let excitedWelcomeMessage = `${currentUser} Welcome to Flatbook, !`.toUpperCase()
+
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
